10fb3b9178bbb4fff1a0f8efb64222cdde35d917
ANHQUAN9982 <quanquytu1024@gmail.com>
2021-06-28 16:52:54 +0700

    them_moi28.6

--- src/index.js
+++ src/index.js
@@ -1,29 +1,44 @@
-const express = require('express')
-const morgan = require('morgan')
-const exhb = require('express-handlebars')
-const path = require('path')
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const exhb = require('express-handlebars');
+const path = require('path');
+const app = express();
 const port = 3000
 
-app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded());
+app.use(express.json());
 //http logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 //template engine
 app.engine('handlebars', exhb());
-app.set('view engine', 'handlebars')
+app.set('view engine', 'handlebars');
 //app.set('views', path.join(__dirname, 'views'));
 
 //console.log('PATH: '+ path.join(__dirname, 'resource/views'))
-app.set('views', path.join(__dirname, 'resource/views'))
+app.set('views', path.join(__dirname, 'resource/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
 })
-app.get('/trang-chinh', (req, res) => {
+
+app.get('/home', (req, res) => {
+  res.render('home');
+})
+app.get('/news', (req, res) => {
   res.render('news');
 })
-
+app.get('/contact', (req, res) => {
+  res.render('contact');
+})
+app.get('/search', (req, res) => {
+  console.log(req.query.q);
+  res.render('search');
+})
+app.post('/search', (req, res) => {
+  console.log(req.body);
+  res.send('');
+})
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
